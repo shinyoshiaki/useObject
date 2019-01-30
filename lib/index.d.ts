1c78d41450d8fb470a5f12a837eaac36953dcade
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-01-30 23:20:34 +0900

    update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,3 +2,7 @@ export default function useObject<T>(obj: T): {
     state: {} & T;
     setState: (next: Partial<T>) => void;
 };
+export declare function alphaUseObject2<T>(obj: T): {
+    state: T;
+    setState: (next: Partial<T>) => void;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,30 @@ export default function useObject<T>(obj: T) {
   const [state, setObj] = useState(Object.assign({}, obj));
 
   const setState = (next: Partial<T>) => {
-    setObj(Object.assign({}, state, next));
+    setObj(prev => {
+      return { ...prev, ...next };
+    });
   };
 
   return { state, setState };
 }
 
+export function alphaUseObject2<T>(obj: T) {
+  const store: { [key: string]: any } = {};
+  Object.keys(obj).forEach(key => {
+    store[key] = useState({ ...(obj as any)[key] });
+  });
+
+  const setState = (next: Partial<T>) => {
+    Object.keys(next).forEach(key => {
+      store[key][1]((next as any)[key]);
+    });
+  };
+
+  const state: any = {};
+  Object.keys(store).forEach(key => {
+    state[key] = store[key][0];
+  });
+
+  return { state: state as T, setState };
+}
